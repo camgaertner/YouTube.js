d94df053c6ce1f4c2ebd20c2df677b0cde655655
Cameron <camgaertner@gmail.com>
2026-03-15 00:41:56 +0800

    fix(Session): Add the ID for ANDROID_VR, remove the mistaken ANDROID_TV key. Enforce key names with TS.

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -102,10 +102,12 @@ export const CLIENTS = {
     STATIC_VISITOR_ID: '6zpwvWUNAco'
   }
 } as const;
+
 /**
  * The keys correspond to the `NAME` fields in {@linkcode CLIENTS} constant
  */
-export const CLIENT_NAME_IDS = {
+type ClientName = typeof CLIENTS[keyof typeof CLIENTS]['NAME'];
+export const CLIENT_NAME_IDS: Record<ClientName, string> = {
   iOS: '5',
   WEB: '1',
   MWEB: '2',
@@ -114,7 +116,7 @@ export const CLIENT_NAME_IDS = {
   ANDROID: '3',
   ANDROID_CREATOR: '14',
   ANDROID_MUSIC: '21',
-  ANDROID_TV: '28',
+  ANDROID_VR: '28',
   TVHTML5: '7',
   TVHTML5_SIMPLY: '74',
   TVHTML5_SIMPLY_EMBEDDED_PLAYER: '85',
